bb10f5285e3f5d68ff6cd8da4f3819bfe721b840
Jim O'Regan <jaoregan@tcd.ie>
2021-08-10 22:49:05 +0100

    more tweaks, still not working

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # coding=UTF-8
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='pygramadan',
     python_requires='>3.7.0',
-    packages=['pygramadan', 'test'],
+    packages=find_packages(),
     version='0.0.1',
     description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
     author='',
