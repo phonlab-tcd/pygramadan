72a60144d9f9da71716acf4bb4883cb2033cc16d
Jim O'Regan <jaoregan@tcd.ie>
2021-08-10 23:17:01 +0100

    start next test

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 
 setup(
     name='pygramadan',
-    python_requires='>3.7.0',
     packages=find_packages(),
     version='0.0.1',
     description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
--- tests/test_opers.py
+++ tests/test_opers.py
@@ -1,4 +1,4 @@
-from pygramadan.opers import lenition
+from pygramadan.opers import *
 
 def test_lenition():
     assert lenition("deas") == 'dheas'
@@ -10,3 +10,11 @@ def test_lenition():
     assert lenition("djeas") == 'djeas'
     assert lenition("Djeas") == 'Djeas'
     assert lenition("DJEAS") == 'DJEAS'
+
+def test_demutate():
+    assert demutate('gcat') == 'cat'
+    assert demutate('chat') == 'cat'
+    assert demutate('mballa') == 'balla'
+    assert demutate('bhalla') == 'balla'
+    assert demutate("d'fhan") == 'fan'
+    assert demutate("d'oscail") == 'oscail'
