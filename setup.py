540e684af28fd8e9b9ba2e85165aba8b4cc9d2a9
Jim O'Regan <jaoregan@tcd.ie>
2021-08-10 21:39:53 +0100

    incomplete library pieces

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pygramadan',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
+    author='',
+    license='MIT',
+)
