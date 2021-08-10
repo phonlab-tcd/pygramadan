6fce082eb6087ac12e66b9d5596adcb0ec596821
Jim O'Regan <jaoregan@tcd.ie>
2021-08-10 22:27:56 +0100

    more setup pieces

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+# coding=UTF-8
 from setuptools import find_packages, setup
 
 setup(
     name='pygramadan',
-    packages=find_packages(),
+    python_requires='>3.7.0',
+    packages=['pygramadan', 'test'],
     version='0.0.1',
     description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
     author='',
     license='MIT',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+          'lxml',
+          'pytest'
+      ],
 )
