4bae6801509bd49d40944a9842c9299f2316b827
Jim O'Regan <jaoregan@tcd.ie>
2021-08-10 23:09:56 +0100

    split up more

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,12 +1,18 @@
 from pygramadan.forms import Form, FormPl, FormSg
 from pygramadan.attributes import Gender, Strength
 
+# this is more about making sure that dataclasses works
+# there isn't really anything to test otherwise
 def test_forms():
     form = Form("sampla")
     assert form.value == "sampla" 
+
+def test_formsg():
     formf = FormSg("traein", Gender.Fem)
     assert formf.value == "traein"
     assert formf.gender == Gender.Fem
+
+def test_formpl():
     formpl = FormPl("traenacha", Strength.Strong)
     assert formpl.value == "traenacha"
     assert formpl.strength == Strength.Strong
