400f7ea09a968360a4e6dc7a9902f7985f239295
Jim O'Regan <jaoregan@tcd.ie>
2021-08-11 22:13:30 +0100

    example was missing an attribute; test would fail

--- tests/test_noun.py
+++ tests/test_noun.py
@@ -6,7 +6,7 @@ from lxml.doctestcompare import LXMLOutputChecker, PARSE_XML
 
 AINM_XML_HEADER = """
 <?xml version='1.0' encoding='utf-8'?>
-<noun default="ainm" declension="4" disambig="" isProper="0" isDefinite="0" allowArticledGenitive="0">
+<noun default="ainm" declension="4" disambig="" isProper="0" isDefinite="0" allowArticledGenitive="0" isImmutable="0">
   <sgNom default="ainm" gender="masc" />
   <sgGen default="ainm" gender="masc" />
   <plNom default="ainmneacha" />
@@ -15,7 +15,7 @@ AINM_XML_HEADER = """
 """
 
 AINM_XML = """
-<noun default="ainm" declension="4" disambig="" isProper="0" isDefinite="0" allowArticledGenitive="0">
+<noun default="ainm" declension="4" disambig="" isProper="0" isDefinite="0" allowArticledGenitive="0" isImmutable="0">
   <sgNom default="ainm" gender="masc" />
   <sgGen default="ainm" gender="masc" />
   <plNom default="ainmneacha" />
