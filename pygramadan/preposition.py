270bda907ba70a7770cbb9a7a73869ef21f34233
Jim O'Regan <jaoregan@tcd.ie>
2021-08-12 19:43:31 +0100

    this is the actual logic

--- pygramadan/preposition.py
+++ pygramadan/preposition.py
@@ -39,13 +39,13 @@ class Preposition:
         return f'{self.get_lemma().replace(" ", "_")}_prep{disambig}'
 
     def is_empty(self) -> bool:
-        return len(self.sg1) == 0 and \
-            len(self.sg2) == 0 and \
-            len(self.sg3_masc) == 0 and \
-            len(self.sg3_fem) == 0 and \
-            len(self.pl1) == 0 and \
-            len(self.pl2) == 0 and \
-            len(self.pl3) == 0
+        return (len(self.sg1) + \
+            len(self.sg2) + \
+            len(self.sg3_masc) + \
+            len(self.sg3_fem) + \
+            len(self.pl1) + \
+            len(self.pl2) + \
+            len(self.pl3)) == 0
 
     def get_lemma(self):
         return self.lemma
