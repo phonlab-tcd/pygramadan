2283cc5778953dac9946cc0955dd4c8621af3e67
Jim O'Regan <jaoregan@tcd.ie>
2021-08-12 16:42:42 +0100

    sucking it up and doing the crappy initialisation

--- pygramadan/verb.py
+++ pygramadan/verb.py
@@ -1,8 +1,10 @@
-from .attributes import Mutation as M, VerbPerson
+# coding=UTF-8
+from .attributes import Mutation as M
 from .attributes import VerbTense as VT
 from .attributes import VerbDependency as VD
 from .attributes import VerbMood as VM
-
+from .attributes import VerbPerson as VPN
+from .attributes import VPMood, VPPerson, VPPolarity, VPShape, VPTense
 
 class VerbTenseRule:
     def __init__(self,
@@ -10,7 +12,7 @@ class VerbTenseRule:
                  mutation: M = M.NoMut,
                  tense: VT = VT.Pres,
                  dependency: VD = VD.Indep,
-                 person: VerbPerson = VerbPerson.Base,
+                 person: VPN = VPN.Base,
                  pronoun: str = "") -> None:
         self.particle = particle
         self.mutation = mutation
@@ -18,4 +20,28 @@ class VerbTenseRule:
         self.dependency = dependency
         self.person = person
         self.pronoun = pronoun
-        
+
+
+class Verb:
+    def __init__(self) -> None:
+
+        # ewwwwww
+        self.tense_rules = {}
+        for tense in VPTense:
+            if tense == VPTense.Any:
+                continue
+            self.tense_rules[tense] = {}
+            for person in VPPerson:
+                if person == VPPerson.Any:
+                    continue
+                self.tense_rules[tense][person] = {}
+                for shape in VPShape:
+                    if shape == VPShape.Any:
+                        continue
+                    self.tense_rules[tense][person][shape] = {}
+                    for polarity in VPPolarity:
+                        if polarity == VPPolarity.Any:
+                            continue
+                        self.tense_rules[tense][person][shape][polarity] = []
+
+            
