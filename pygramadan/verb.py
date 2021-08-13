b56a87832bc9e01c34a758b2b368375fd019f863
Jim O'Regan <jaoregan@tcd.ie>
2021-08-13 19:07:51 +0100

    start to_xml

--- pygramadan/verb.py
+++ pygramadan/verb.py
@@ -7,11 +7,24 @@ from .attributes import VerbPerson as VPN
 from .attributes import VPMood, VPPerson, VPPolarity, VPShape, VPTense
 from .verb_tense_rule import VerbTenseRule
 from .default_tense_rules import get_default_tense_rules
-
+import xml.etree.ElementTree as ET
+from typing import List
+from .forms import Form
 
 class Verb:
-    def __init__(self) -> None:
+    def __init__(self,
+                 source = None,
+                 verbal_noun: List[Form] = None,
+                 verbal_adj: List[Form] = None
+        ) -> None:
         self.tense_rules = get_default_tense_rules()
+        self.verbal_noun: List[Form] = verbal_noun
+        self.verbal_adj: List[Form] = verbal_adj
+
+        if self.verbal_noun == None:
+            self.verbal_noun = []
+        if self.verbal_adj == None:
+            self.verbal_adj = []
 
     def get_tense_rules(self, tense: VPTense, person: VPPerson, shape: VPShape, polarity: VPPolarity):
         out = []
@@ -37,3 +50,16 @@ class Verb:
                             for rule in self.tense_rules[t][per][s][pol]:
                                 out.append(rule)
         return out
+
+    def from_xml(self, source):
+        pass
+
+    def to_xml(self):
+        props = {}
+        props['default'] = self.get_lemma()
+        props['disambig'] = self.disambig
+        root = ET.Element('noun', props)
+        for form in self.verbal_noun:
+            _ = ET.SubElement(root, 'verbalNoun', {'default': form.value})
+        for form in self.verbal_adj:
+            _ = ET.SubElement(root, 'verbalAdjective', {'default': form.value})
