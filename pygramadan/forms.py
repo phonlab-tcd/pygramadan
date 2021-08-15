93a24f1b4dcba9d9b970a06ac4145154c8df3768
Jim Regan <jaoregan@tcd.ie>
2021-08-15 14:51:25 +0100

    basic class docstrings

--- pygramadan/forms.py
+++ pygramadan/forms.py
@@ -2,17 +2,20 @@ from .attributes import Gender, Strength
 
 
 class Form:
+    """A basic word form, containing only a string value"""
     def __init__(self, value: str) -> None:
         self.value: str = value
 
 
 class FormSg(Form):
+    """A singular noun form, containing a string value, and the grammatical gender of that form"""
     def __init__(self, value: str, gender: Gender) -> None:
         self.value: str = value
         self.gender: Gender = gender
 
 
 class FormPlGen(Form):
+    """A genitive plural form, containing a string value, and the 'strength' of the form"""
     def __init__(self, value: str, strength: Strength) -> None:
         self.value: str = value
         self.strength: Strength = strength
