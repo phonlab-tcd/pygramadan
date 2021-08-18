1d5503fd170769a37d3264c6c3ef8e28e9eb4173
Jim O'Regan <jaoregan@tcd.ie>
2021-08-18 09:15:30 +0100

    ar

--- pygramadan/prepositional_phrase.py
+++ pygramadan/prepositional_phrase.py
@@ -84,4 +84,24 @@ iolra, alt:                      {", ".join(self.pl_art)}
             for f in np.pl_dat_art:
                 value = mutate(Mutation.PrefH, f.value)
                 self.pl_art.append(Form(f'ag na {value}'))
+        if self.prep_id == 'ar_prep':
+            for f in np.sg_dat:
+                value = mutate(Mutation.Len1, f.value)
+                self.sg.append(FormSg(f'ar {value}', f.gender))
+            for f in np.pl_dat:
+                value = mutate(Mutation.Len1, f.value)
+                self.pl.append(Form(f'ar {value}'))
+            for f in np.sg_dat_art_n:
+                value = mutate(Mutation.Len3, f.value)
+                self.sg_art_n.append(FormSg(f'ar an {value}', f.gender))
+            for f in np.sg_dat_art_s:
+                if f.gender == Gender.Fem:
+                    mut = Mutation.Ecl3
+                else:
+                    mut = Mutation.Ecl2
+                value = mutate(mut, f.value)
+                self.sg_art_s.append(FormSg(f'ar an {value}', f.gender))
+            for f in np.pl_dat_art:
+                value = mutate(Mutation.PrefH, f.value)
+                self.pl_art.append(Form(f'ar na {value}'))
 
