1e38764f2e9660eb7443ea7b81aad42596a779ff
Jim O'Regan <jaoregan@tcd.ie>
2021-08-18 20:37:18 +0100

    spacing

--- tests/test_verb_phrase.py
+++ tests/test_verb_phrase.py
@@ -8,9 +8,11 @@ import io
 _sio = io.StringIO(AIMSIGH_XML_FULL)
 _aimsigh = Verb(source=_sio)
 
+
 def test_init():
     aimsigh_vp = VP(_aimsigh)
     assert aimsigh_vp is not None
+
 
 _PAST_DEC_NEG = """Sg1: [níor aimsigh mé] 
 Sg2: [níor aimsigh tú] 
@@ -22,6 +24,7 @@ Pl3: [níor aimsigh siad] [níor aimsíodar]
 NoSubject: [níor aimsigh] 
 Auto: [níor aimsíodh] 
 """
+
 
 def test_print_tenses():
     aimsigh_vp = VP(_aimsigh)
