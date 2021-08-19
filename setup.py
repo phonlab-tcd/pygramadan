e5ae26e9ca0c5bb9fe6897cab429c68f1774d9c6
Jim O'Regan <jaoregan@tcd.ie>
2021-08-19 11:14:36 +0100

    Revert "specify manually"
    This reverts commit d6770d46142346ba77004fe33543a5b1ed37f054.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # coding=UTF-8
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='pygramadan',
-    packages=['pygramadan', 'test'],
+    packages=find_packages(),
     version='0.0.1',
     description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
     author='',
