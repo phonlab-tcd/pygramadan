d7f0552b7af1d3b74b9e126211877af2611d448c
Jim O'Regan <jaoregan@tcd.ie>
2021-08-19 11:15:12 +0100

    try this way

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pygramadan',
-    packages=find_packages(),
+    packages=find_packages(exclude=("scripts",)),
     version='0.0.1',
     description='Library for working with Bunachar Náisiúnta Moirfeolaíochta',
     author='',
