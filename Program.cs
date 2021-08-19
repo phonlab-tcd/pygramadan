e43e343bb778d9d9f4ad5e3eb54011b940925ccb
Jim O'Regan <jaoregan@tcd.ie>
2021-08-19 18:31:47 +0100

    use generated version of verb xml

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Gramadan;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Tester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PrinterNeid printer=new PrinterNeid(false);
+			XmlDocument doc = new XmlDocument();
+			doc.Load("aimsigh_verb.xml");
+			Verb verb = new Verb(doc);
+			StreamWriter writer = new StreamWriter("aimsigh_print.xml");
+			writer.WriteLine(printer.printVerbXml(verb));
+			writer.Close();
+		}
+	}
+}
