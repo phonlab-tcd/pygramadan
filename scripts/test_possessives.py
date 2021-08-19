f8f6344127a1722519ef1ef09dfe497888f76c58
Jim O'Regan <jaoregan@tcd.ie>
2021-08-19 10:19:12 +0100

    cat tmp|awk -F'\\' '{print $NF}'|awk -F'"' '{print "    \"" $1 "\","}'

---
+++ scripts/test_possessives.py
@@ -0,0 +1,43 @@
+import argparse
+
+
+_NOUNS = [
+    "árasán_masc1.xml",
+    "bó_fem.xml",
+    "comhlacht_masc3.xml",
+    "dealbh_fem2.xml",
+    "éiceachóras_masc1.xml",
+    "francfurtar_masc1.xml",
+    "fliúit_fem2.xml",
+    "fadhb_fem2.xml",
+    "fobhríste_masc4.xml",
+    "garáiste_masc4.xml",
+    "haematóma_masc4.xml",
+    "iasacht_fem3.xml",
+    "jab_masc4.xml",
+    "leabharlann_fem2.xml",
+    "máthair_fem5.xml",
+    "nóta_masc4.xml",
+    "ócáid_fem2.xml",
+    "pacáiste_masc4.xml",
+    "rás_masc3.xml",
+    "sobaldráma_masc4.xml",
+    "sábh_masc1.xml",
+    "stábla_masc4.xml",
+    "sráid_fem2.xml",
+    "tábhairne_masc4.xml",
+    "ubh_fem2.xml",
+    "x-gha_masc4.xml",
+    "zombaí_masc4.xml"
+]
+
+
+_PREPS = [
+    "mo_poss.xml",
+    "do_poss.xml",
+    "a_poss_masc.xml",
+    "a_poss_fem.xml",
+    "ár_poss.xml",
+    "bhur_poss.xml",
+    "a_poss_pl.xml"
+]
