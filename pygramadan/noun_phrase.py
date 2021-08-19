bca0d1f2422514773642b82fdc75468de21953c7
Jim O'Regan <jaoregan@tcd.ie>
2021-08-19 10:57:03 +0100

    handle plurals

--- pygramadan/noun_phrase.py
+++ pygramadan/noun_phrase.py
@@ -426,15 +426,21 @@ class NP():
         def starts_v(txt: str) -> bool:
             return starts_vowel(txt) or starts_fvowel(txt)
 
-        def _do_forms(inlist, outlist):
+        def _do_forms(inlist, outlist, plural = False):
             for form in inlist:
                 value = mutate(poss.mutation, form.value)
                 if len(poss.apos) > 0 and starts_v(form.value):
                     for possform in poss.apos:
-                        outlist.append(FormSg(f'{possform.value}{value}', form.gender))
+                        if plural:
+                            outlist.append(Form(f'{possform.value}{value}'))
+                        else:
+                            outlist.append(FormSg(f'{possform.value}{value}', form.gender))
                 else:
                     for possform in poss.full:
-                        outlist.append(FormSg(f'{possform.value} {value}', form.gender))
+                        if plural:
+                            outlist.append(Form(f'{possform.value} {value}'))
+                        else:
+                            outlist.append(FormSg(f'{possform.value} {value}', form.gender))
 
         self.is_definite = noun.is_definite
 
