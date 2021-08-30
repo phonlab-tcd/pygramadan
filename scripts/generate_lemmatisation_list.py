ce92eab3c4c66aecfc6c6f55ff0b2d102febca3b
Jim O'Regan <jaoregan@tcd.ie>
2021-08-30 11:56:18 +0100

    start script

---
+++ scripts/generate_lemmatisation_list.py
@@ -0,0 +1,38 @@
+import argparse
+from pygramadan.noun import Noun
+from pygramadan.adjective import Adjective
+from pathlib import Path
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bunamo", type=str, help="path to BuNaMo")
+    args = parser.parse_args()
+    if args.bunamo is None:
+        sys.exit('--bunamo option not set')
+    bunamo = Path(args.bunamo)
+    if not bunamo.is_dir():
+        sys.exit(f'path "{args.bunamo}" is not a directory')
+    noun_dir = bunamo / 'noun'
+    if not noun_dir.is_dir():
+        sys.exit(f'"{args.bunamo}" does not contain noun/ directory')
+    adj_dir = bunamo / 'adjective'
+    if not adj_dir.is_dir():
+        sys.exit(f'"{args.bunamo}" does not contain adjective/ directory')
+    verb_dir = bunamo / 'verb'
+    if not verb_dir.is_dir():
+        sys.exit(f'"{args.bunamo}" does not contain verb/ directory')
+    prep_dir = bunamo / 'preposition'
+    if not prep_dir.is_dir():
+        sys.exit(f'"{args.bunamo}" does not contain preposition/ directory')
+
+    nouns = {}
+    for noun_file in noun_dir.glob('*.xml'):
+        cur_noun = Noun(source=noun_file)
+        cur_lem = cur_noun.get_lemma()
+        if cur_lem not in nouns:
+            nouns[cur_lem] = set()
+        for form in cur_noun.sg_nom:
+            if form.value != cur_lem:
+                nouns[cur_lem].add(form.value)
