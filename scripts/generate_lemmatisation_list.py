b7bc303fef20e1980a4e97c91f8944ca74c6d2af
Jim O'Regan <jaoregan@tcd.ie>
2021-08-31 11:18:16 +0100

    generate noun json. Unfortunately, backwards, so rewrite

--- scripts/generate_lemmatisation_list.py
+++ scripts/generate_lemmatisation_list.py
@@ -2,6 +2,7 @@ import argparse
 from pygramadan.noun import Noun
 from pygramadan.adjective import Adjective
 from pathlib import Path
+import json
 import sys
 
 
@@ -33,6 +34,18 @@ def main():
         cur_lem = cur_noun.get_lemma()
         if cur_lem not in nouns:
             nouns[cur_lem] = set()
-        for form in cur_noun.sg_nom:
-            if form.value != cur_lem:
-                nouns[cur_lem].add(form.value)
+        for form in cur_noun.get_unique_forms():
+            if form != cur_lem:
+                nouns[cur_lem].add(form)
+    for noun in nouns.keys():
+        if len(nouns[noun]) == 1:
+            nouns[noun] = list(nouns[noun])[0]
+        else:
+            nouns[noun] = list(nouns[noun])
+    
+    with open('ga_lemma_lookup_noun.json', 'w') as out:
+        json.dump(nouns, out)
+
+
+if __name__ == "__main__":
+    main()
