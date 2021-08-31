bf1beda2a10034f5fcbfea3de2465ceabad8c768
Jim O'Regan <jaoregan@tcd.ie>
2021-08-31 13:00:47 +0100

    more noun xml samples

--- pygramadan/samples.py
+++ pygramadan/samples.py
@@ -1,3 +1,6 @@
+from tests.test_noun import AINM_XML_HEADER
+
+
 _HEADER = """
 <?xml version='1.0' encoding='utf-8'?>
 """
@@ -14,3 +17,16 @@ BEAG_XML = """
 """
 
 BEAG_XML_HEADER = _HEADER + BEAG_XML
+
+
+AINM_XML = """
+<noun default="ainm" declension="4" disambig="" isProper="0" isDefinite="0" allowArticledGenitive="0" isImmutable="0">
+  <sgNom default="ainm" gender="masc" />
+  <sgGen default="ainm" gender="masc" />
+  <plNom default="ainmneacha" />
+  <plGen default="ainmneacha" strength="strong" />
+</noun>
+"""
+
+
+AINM_XML_HEADER = _HEADER + AINM_XML
