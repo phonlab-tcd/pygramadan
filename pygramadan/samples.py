02d34d0365a68423fee8856a252153ad0e74fd10
Jim O'Regan <jaoregan@tcd.ie>
2021-08-31 12:58:43 +0100

    move sample XML to a more central place

---
+++ pygramadan/samples.py
@@ -0,0 +1,16 @@
+_HEADER = """
+<?xml version='1.0' encoding='utf-8'?>
+"""
+
+BEAG_XML = """
+<adjective default="beag" declension="1" disambig="" isPre="0">
+  <sgNom default="beag" />
+  <sgGenMasc default="big" />
+  <sgGenFem default="bige" />
+  <plNom default="beaga" />
+  <graded default="lú" />
+  <abstractNoun default="laghad" />
+</adjective>
+"""
+
+BEAG_XML_HEADER = _HEADER + BEAG_XML
